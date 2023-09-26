2477839a3ecf8d4656bf5b6f5c40889f2006037b
rejuancse <rejuancse@gmail.com>
2023-09-26 19:53:43 +0600

    For use Count: Decrement btn added

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,5 +1,17 @@
-function CounterPage() {
-    return <div>Counter</div>;
+import Button from '../components/Button';
+import useCounter from '../hooks/use-counter';
+
+function CounterPage({initialCount}) {
+    const { count, increment, decrement } = useCounter(initialCount);
+
+    return <div>
+        <h2>Count is: {count}</h2>
+
+        <div className="flex">
+            <Button onClick={increment}>Increment</Button>
+            <Button onClick={decrement}>Decrement</Button>
+        </div>
+    </div>
 }
 
 export default CounterPage;
