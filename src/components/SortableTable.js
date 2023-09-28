81bf60b1d3e2b35cba9efd0f5d10098500f8dc80
rejuancse <rejuancse>
2023-09-28 21:21:52 +0600

    Counter component script updated

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,24 +1,16 @@
-import { useState } from "react";
 import { GoTriangleDown } from 'react-icons/go';
 import Table from "./Table";
+import useSort from '../hooks/use-sort';
 
 function SortableTable(props) {
-    const [sortOrder, setSortOrder] = useState(null);
-    const [sortBy, setSortBy] = useState(null);
     const { config, data } = props;
 
-    const handleClick = (label) => {
-        if (sortOrder === null) {
-            setSortOrder('asc');
-            setSortBy(label);
-        } else if (sortOrder === 'asc') {
-            setSortOrder('desc');
-            setSortBy(label);
-        } else if (sortOrder === 'desc') {
-            setSortOrder(null);
-            setSortBy(null);
-        }
-    };
+    const {
+        // sortOrder,
+        // sortBy,
+        sortedData,
+        setSortColumn
+    } = useSort(data, config);
 
     const updatedConfig = config.map((column) => {
         if(!column.sortValue) {
@@ -27,25 +19,9 @@ function SortableTable(props) {
 
         return {
             ...column,
-            header: () => <th onClick={() => handleClick(column.label)} className="flex items-center cursor-pointer border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-400 text-left">{column.label} <GoTriangleDown /></th>
+            header: () => <th onClick={() => setSortColumn(column.label)} className="flex items-center cursor-pointer border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-400 text-left">{column.label} <GoTriangleDown /></th>
         }
     });
-
-    let sortedData = data;
-    if (sortOrder && sortBy) {
-        const { sortValue } = config.find((column) => column.label === sortBy);
-        sortedData = [...data].sort((a, b) => {
-            const valueA = sortValue(a);
-            const valueB = sortValue(b);
-            const reverseOrder = sortOrder === 'asc' ? 1 : -1;
-
-            if (typeof valueA === 'string') {
-                return valueA.localeCompare(valueB) * reverseOrder;
-            } else {
-                return (valueA - valueB) * reverseOrder;
-            }
-        });
-    }
 
     return (
         <div>
--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,17 +1,44 @@
+import { useState } from 'react';
 import Button from '../components/Button';
 import useCounter from '../hooks/use-counter';
+import Panel from '../components/Panel';
 
 function CounterPage({initialCount}) {
-    const { count, increment, decrement } = useCounter(initialCount);
+    const [count, setCount] = useState(initialCount);
+    const { increment, decrement } = useCounter(initialCount);
+    const [ valueToAdd, setValueToAdd ] = useState(0);
 
-    return <div>
-        <h2>Count is: {count}</h2>
+    const handleChange = (event) => {
+        const value = parseInt(event.target.value) || 0;
+        setValueToAdd(value);
+    }
 
-        <div className="flex">
-            <Button onClick={increment}>Increment</Button>
-            <Button onClick={decrement}>Decrement</Button>
-        </div>
-    </div>
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        setCount(count+valueToAdd);
+        setValueToAdd(0)
+    }
+
+    return (
+        <Panel className="m-3">
+            <h1 className="text-lg">Count is: {count}</h1>
+            <div className="flex flex-row">
+                <Button onClick={increment}>Increment</Button>
+                <Button onClick={decrement}>Decrement</Button>
+            </div>
+
+            <form onSubmit={handleSubmit}>
+                <label>Add a lot!</label>
+                <input
+                    value={valueToAdd || 0}
+                    onChange={handleChange}
+                    type="number"
+                    className="p-1 m-3 bg-gray-50 border border-gray-300" />
+                <Button primary>Add it!</Button>
+            </form>
+        </Panel>
+    )
 }
 
 export default CounterPage;
